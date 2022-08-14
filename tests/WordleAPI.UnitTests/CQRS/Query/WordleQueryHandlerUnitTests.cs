7609904a1735fe0c253ff523887c792ed11f8663
Joe Dracup <joe@dracup.me.uk>
2022-08-14 11:50:25 +0100

    Added Unit Tests FrameWork

---
+++ tests/WordleAPI.UnitTests/CQRS/Query/WordleQueryHandlerUnitTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WordleAPI.CQRS.Query;
+using WordleAPI.CQRS.QueryHandler;
+using WordleAPI.Repositories.Interfaces;
+using WordleAPI.Services.Interfaces;
+using Xunit;
+
+namespace WordleAPI.UnitTests.CQRS
+{
+    public class WordleQueryHandlerUnitTests
+    {
+        private readonly Mock<IWordleQueryValidator> _validator;
+        private readonly Mock<IWordleService> _service;
+        private readonly Mock<IWordleRepo> _repo;
+        private WordleQueryHandler handler;
+
+        public WordleQueryHandlerUnitTests()
+        {
+            _validator = new Mock<IWordleQueryValidator>();
+            _service = new Mock<IWordleService>();
+            _repo = new Mock<IWordleRepo>();
+
+            handler = new WordleQueryHandler(
+                _validator.Object,
+                _service.Object,
+                _repo.Object
+            );
+        }
+
+        [Fact]
+        public async void Handle_ValidatorFails_ReturnErrorObject()
+        {
+            //Arrange
+            WordleQuery query = new WordleQuery();
+            _validator.Setup(v => v.Validate(query)).Returns(false);
+
+            //Act
+            var result = handler.Handle(query, default);
+
+            //Assert
+            _validator.Verify(v => v.Validate(query), Times.Once);
+            Assert.False(result.Result.Result);
+            Assert.Equal(WordleErrorCode.InvalidRequest, result.Result.ErrorCode);
+        }
+    }
+}
